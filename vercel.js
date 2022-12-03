8aaaa989cbbf1a0ba548206ca940a8bf1a29d3b3
Arthur Ribeker <102258317+ribekerA@users.noreply.github.com>
2022-12-03 15:47:26 -0300

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,16 @@
+
+{
+    "versão" ; 2 ,
+    "cria" ; [
+   {
+    "src" : " ./index.js " ,
+    "use" : " @vercel/node "
+   }
+   ],
+    "rotas" ; [
+   {
+    "src" : " /(.*) " ,
+    "destino" : " / "
+   }
+   ]
+   }
