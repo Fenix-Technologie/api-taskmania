86bdb6de7ba9682b8981e397864a963f103d1d82
IMvErickz <erickspy2003@gmail.com>
2023-06-09 18:50:30 -0300

    get members email and delete passoword request(refine query)

--- models/Board/FindById.js
+++ models/Board/FindById.js
@@ -1,7 +1,10 @@
 const Board = require('.')
 
 const FindById = async (id) =>
-  await Board.findById(id).populate('lists')
+await Board.findById(id).populate('lists').populate({
+  path: 'members.user',
+  select: '-password -boards'
+})
   // .populate('members.user')
 
 module.exports = FindById
