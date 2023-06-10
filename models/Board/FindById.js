b57327529a7d166324fb688fc6ef4a0f6ea20a90
RicardoSobral7 <riki.sobral@gmail.com>
2023-06-10 20:12:24 -0300

    solve populate list and cards

--- models/Board/FindById.js
+++ models/Board/FindById.js
@@ -1,10 +1,15 @@
 const Board = require('.')
 
 const FindById = async (id) =>
-await Board.findById(id).populate('lists').populate({
-  path: 'members.user',
-  select: '-password -boards'
-})
+  await Board.findById(id).populate({
+    path: 'members.user',
+    select: '-password -boards'
+  }).populate([
+    {
+      path: 'lists',
+      populate: [{path: 'cards'}]
+    }
+  ])
 
 
 module.exports = FindById
